fc3bb884d6ef80dbeedce5d05ef6a67e245de16b
SotaSato-stst <102202272+SotaSato-stst@users.noreply.github.com>
2024-05-27 12:49:54 +0900

    main関数の分割が完了。csvからデータを読み込む関数も完了。TODO: データからのトポロジーの生成

--- scratch/sim.cc
+++ scratch/sim.cc
@@ -1,79 +1,94 @@
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation;
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
- */
-
 #include "ns3/applications-module.h"
 #include "ns3/core-module.h"
 #include "ns3/internet-module.h"
 #include "ns3/network-module.h"
 #include "ns3/point-to-point-module.h"
 
-// Default Network Topology
-//
-//       10.1.1.0
-// n0 -------------- n1
-//    point-to-point
-//
-
 using namespace ns3;
+
+#define NET_MASK "255.255.255.0"
+#define FIRST_NO "0.0.0.1"
+#define SOURCE_TOPOLOGY_FILE_NAME "./data/sample.csv"
+
+void generateTopology();
+std::vector<std::vector<int>> readMatrixFromCSV(const std::string& filename);
+void ConfigureCommandLine(int argc, char* argv[]);
+void SetupLogging();
+NodeContainer CreateTopology();
+NetDeviceContainer SetupDataLinkLayer(NodeContainer& nodes);
+Ipv4InterfaceContainer SetupNetworkLayer(NodeContainer& nodes, NetDeviceContainer& devices);
+void InstallApplications(NodeContainer& nodes, Ipv4InterfaceContainer& interfaces);
 
 NS_LOG_COMPONENT_DEFINE("FirstScriptExample");
 
 int
 main(int argc, char* argv[])
 {
+    ConfigureCommandLine(argc, argv);
+    SetupLogging();
+
+    NodeContainer nodes = CreateTopology();
+    NetDeviceContainer devices = SetupDataLinkLayer(nodes);
+    Ipv4InterfaceContainer interfaces = SetupNetworkLayer(nodes, devices);
+    InstallApplications(nodes, interfaces);
+
+    Simulator::Run();
+    Simulator::Destroy();
+    generateTopology();
+
+    return 0;
+}
+
+void
+ConfigureCommandLine(int argc, char* argv[])
+{
     CommandLine cmd(__FILE__);
     cmd.Parse(argc, argv);
+}
 
-    Time::SetResolution(Time::NS);
-    // アプリケーションにログをはかすことができる
+void
+SetupLogging()
+{
     LogComponentEnable("UdpEchoClientApplication", LOG_LEVEL_INFO);
     LogComponentEnable("UdpEchoServerApplication", LOG_LEVEL_INFO);
+}
 
-    // データリンク層(L2)開始
-
+NodeContainer
+CreateTopology()
+{
     NS_LOG_UNCOND("Creating Topology");
-    // NodeContainer = トポロジー生成
     NodeContainer nodes;
     nodes.Create(2);
+    return nodes;
+}
 
-    // PointToPointHelper = リンク属性決定
+NetDeviceContainer
+SetupDataLinkLayer(NodeContainer& nodes)
+{
     PointToPointHelper pointToPoint;
     pointToPoint.SetDeviceAttribute("DataRate", StringValue("5Mbps"));
     pointToPoint.SetChannelAttribute("Delay", StringValue("2ms"));
+    NetDeviceContainer devices = pointToPoint.Install(nodes);
+    return devices;
+}
 
-    // ネットワークデバイスをノードに装着
-    NetDeviceContainer devices;
-    devices = pointToPoint.Install(nodes);
-
-    // データリンク(L2)層終了
-
-    // ネットワーク層(L3)開始
-
-    // プロトコルスタックをデバイスに乗せる
+Ipv4InterfaceContainer
+SetupNetworkLayer(NodeContainer& nodes, NetDeviceContainer& devices)
+{
     InternetStackHelper stack;
     stack.Install(nodes);
 
-    // ネトワークアドレス決め
     Ipv4AddressHelper address;
     address.SetBase("10.1.1.0", "255.255.255.0");
 
     Ipv4InterfaceContainer interfaces = address.Assign(devices);
+    return interfaces;
+}
 
+void
+InstallApplications(NodeContainer& nodes, Ipv4InterfaceContainer& interfaces)
+{
     UdpEchoServerHelper echoServer(9);
-
-    // アプリケーションをノードに装着
     ApplicationContainer serverApps = echoServer.Install(nodes.Get(1));
     serverApps.Start(Seconds(1.0));
     serverApps.Stop(Seconds(10.0));
@@ -86,45 +101,53 @@ main(int argc, char* argv[])
     ApplicationContainer clientApps = echoClient.Install(nodes.Get(0));
     clientApps.Start(Seconds(2.0));
     clientApps.Stop(Seconds(10.0));
-
-    Simulator::Run();
-    Simulator::Destroy();
 }
 
-// void
-// generateTopology()
-// {
-//     std::string csvfilename = "data.csv";
-//     std::vector<std::vector<int>> data = readCSV(csvfilename);
-// }
+void
+generateTopology()
+{
+    std::string csvfilename = SOURCE_TOPOLOGY_FILE_NAME;
+    std::vector<std::vector<int>> data = readMatrixFromCSV(csvfilename);
 
-// std::vector<std::vector<int>>
-// readCSV(const std::string& filename)
-// {
-//     std::vector<std::vector<int>> data;
-//     std::ifstream file(filename);
-//     std::string line;
+    for (size_t i = 0; i < data.size(); ++i)
+    {
+        for (size_t j = 0; j < data[i].size(); ++j)
+        {
+            if (data[i][j] == 1)
+            {
+                std::cout << "data[" << i << "][" << j << "] = " << data[i][j] << std::endl;
+            }
+        }
+    }
+}
 
-//     if (!file.is_open())
-//     {
-//         std::cerr << "Unable to open file " << filename << std::endl;
-//         return data;
-//     }
+std::vector<std::vector<int>>
+readMatrixFromCSV(const std::string& filename)
+{
+    std::vector<std::vector<int>> data;
+    std::ifstream file(filename);
+    std::string line;
 
-//     while (getline(file, line))
-//     {
-//         std::stringstream lineStream(line);
-//         std::string cell;
-//         std::vector<int> row;
+    if (!file.is_open())
+    {
+        std::cerr << "Unable to open file " << filename << std::endl;
+        return data;
+    }
 
-//         while (getline(lineStream, cell, ','))
-//         {
-//             row.push_back(std::stoi(cell));
-//         }
+    while (getline(file, line))
+    {
+        std::stringstream lineStream(line);
+        std::string cell;
+        std::vector<int> row;
 
-//         data.push_back(row);
-//     }
+        while (getline(lineStream, cell, ','))
+        {
+            row.push_back(std::stoi(cell));
+        }
 
-//     file.close();
-//     return data;
-// }
+        data.push_back(row);
+    }
+
+    file.close();
+    return data;
+}
